f56ba844b0b04c10dd326d072abea9e7756ef822
anandspaces <anandvaibhaw000@gmail.com>
2026-01-08 00:46:37 +0530

    update port and package script

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = 9001;
 
 // Initialize Client using the Token from .env
 const client = new InferenceClient(process.env.HF_TOKEN);
